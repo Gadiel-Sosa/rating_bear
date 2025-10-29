4c5a70c3de3e35dbd5439aa6eabf33e1fe3a5c65
Gadiel Sosa <sosachanantonio0@gmail.com>
2025-10-28 19:23:19 -0600

    rating_animation

--- lib/screens/rate_screen.dart
+++ lib/screens/rate_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_rating/flutter_rating.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:rive/rive.dart';
 
@@ -12,12 +11,42 @@ class RateScreen extends StatefulWidget {
 
 class _RateScreenState extends State<RateScreen> {
   StateMachineController? controller;
-  int starCount = 5;
+  SMINumber? numLookInput;
+  SMITrigger? successInput;
+  SMITrigger? failInput;
   double rating = 0.0;
+  int itemCount = 5;
+  double itemSize = 55;
+
+  //numLookInput?.value = 50.0;
+
+  //void _updateNumLook(double rating) {
+  //  if (numLookInput == null) return;
+  //
+  //  if (rating >= 4.0) {
+  //    numLookInput!.value = 2;
+  //  } else if (rating >= 2.5) {
+  //    numLookInput!.value = 1;
+  //  } else {
+  //    numLookInput!.value = 0;
+  //  }
+  //}
+
+  void _rateNow() {
+    if (controller == null) return;
+
+    //_updateNumLook(rating);
+
+    if (rating >= 3) {
+      successInput?.fire();
+    } else {
+      failInput?.fire();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
+    final size = MediaQuery.of(context).size;
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -30,18 +59,31 @@ class _RateScreenState extends State<RateScreen> {
                 height: 200,
                 child: RiveAnimation.asset(
                   'assets/animated_login_character.riv',
-                  stateMachines: ["Login Machine"],
+                  stateMachines: const ["Login Machine"],
                   onInit: (artboard) {
-                    // Configuramos la conexión entre Flutter y la animación de Rive
                     controller = StateMachineController.fromArtboard(
                       artboard,
                       "Login Machine",
                     );
+
+                    if (controller != null) {
+                      artboard.addController(controller!);
+
+                      numLookInput =
+                          controller!.findInput<double>('numLook')
+                              as SMINumber?;
+                      successInput =
+                          controller!.findInput<bool>('trigSuccess')
+                              as SMITrigger?;
+                      failInput =
+                          controller!.findInput<bool>('trigFail')
+                              as SMITrigger?;
+                    }
                   },
                 ),
               ),
-              SizedBox(height: 20),
-              Text(
+              const SizedBox(height: 20),
+              const Text(
                 'Enjoying Sounter?',
                 textAlign: TextAlign.center,
                 style: TextStyle(
@@ -51,76 +93,58 @@ class _RateScreenState extends State<RateScreen> {
                   color: Colors.black87,
                 ),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               Text(
-                'With how many stars do!',
+                'With how many stars do',
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  color: Colors.grey.shade600,
+                  color: Colors.grey,
                   fontFamily: 'Arial',
                   fontSize: 20,
                 ),
               ),
+              
               Text(
-                'do you rate your experience.',
+                'you rate your experience',
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  color: Colors.grey.shade600,
+                  color: Colors.grey,
                   fontFamily: 'Arial',
                   fontSize: 20,
                 ),
               ),
+              
               Text(
                 'Tap a star to rate!',
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  color: Colors.grey.shade600,
+                  color: Colors.grey,
                   fontFamily: 'Arial',
                   fontSize: 20,
                 ),
               ),
-              SizedBox(height: 20),
-              //StarRating(
-              //  size: 55,
-              //  color: Colors.orange,
-              //  borderColor: Colors.grey,
-              //  allowHalfRating: true,
-              //  starCount: starCount,
-              //  rating: rating,
-              //  onRatingChanged: (newRating) {
-              //    setState(() {
-              //      if (rating == newRating) {
-              //        rating = 0.0;
-              //      } else {
-              //        rating = newRating;
-              //      }
-              //    });
-              //  },
-              //),
-              Center(
-                child: RatingBar.builder(
-                  initialRating: rating,
-                  minRating: 0,
-                  direction: Axis.horizontal,
-                  allowHalfRating: true,
-                  glow: false,
-                  itemCount: 5,
-                  itemSize: 55, 
-                  unratedColor: Colors.grey.shade300,
-                  itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
-                  itemBuilder:
-                      (context, _) =>
-                          const Icon(Icons.star, color: Colors.orange),
-                  onRatingUpdate: (newRating) {
-                    setState(() {
-                      rating = newRating;
-                    });
-                  },
-                ),
+              const SizedBox(height: 20),
+              RatingBar.builder(
+                initialRating: rating,
+                minRating: 0,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                glow: false,
+                itemCount: itemCount,
+                itemSize: itemSize,
+                unratedColor: Colors.grey.shade300,
+                itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                itemBuilder:
+                    (context, _) =>
+                        const Icon(Icons.star, color: Colors.orange),
+                onRatingUpdate: (newRating) {
+                  setState(() => rating = newRating);
+                  //_updateNumLook(rating);
+                },
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               MaterialButton(
-                onPressed: () {},
+                onPressed: _rateNow,
                 color: Colors.deepPurpleAccent,
                 textColor: Colors.white,
                 elevation: 2,
@@ -131,12 +155,12 @@ class _RateScreenState extends State<RateScreen> {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(8),
                 ),
-                child: Text(
+                child: const Text(
                   'Rate Now',
                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               TextButton(
                 onPressed: () {},
                 child: const Text(
